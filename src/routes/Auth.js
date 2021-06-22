7aaea47ba199c8c5344851e6aa80f2fca0417933
giyun-cloud <nangiyoon@gmail.com>
2021-06-22 09:30:03 +0900

    2.0 Using Firebase Auth ~ 2.3 Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { authService } from "fbase";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") setEmail(value);
+    else if (name === "password") setPassword(value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    try {
+      if (newAccount)
+        authService.createUserWithEmailAndPassword(email, password);
+      else authService.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          required
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          type="password"
+          required
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 
 export default Auth;
